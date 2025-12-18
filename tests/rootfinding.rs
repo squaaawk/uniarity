8920c848a5661acc1c7a23e8e1e6b98d5f36097b
squaaawk <81951801+xorshift@users.noreply.github.com>
2025-12-18 15:46:00 -0800

    feat: use automatic differentiation instead of approximations in test cases

--- tests/rootfinding.rs
+++ tests/rootfinding.rs
@@ -1,4 +1,5 @@
 use approx::assert_abs_diff_eq;
+use autodiff::{Float, F1};
 use std::f64::consts::TAU;
 
 use uniarity::bracket::{bisection, itp};
@@ -6,65 +7,85 @@ use uniarity::cheb::Cheb;
 use uniarity::initial::{laguerres_method, newtons_method, secant};
 
 struct TestCase {
-  function: fn(f64) -> f64,
+  function: fn(F1) -> F1,
   a: f64,
   b: f64,
   n: usize,
   low_precision: bool,
 }
 
+impl TestCase {
+  fn f(&self) -> impl Fn(f64) -> f64 + '_ {
+    |x: f64| (self.function)(F1::cst(x)).value()
+  }
+
+  fn fp(&self) -> impl Fn(f64) -> f64 + '_ {
+    |x: f64| (self.function)(F1::var(x)).deriv()
+  }
+
+  fn fpp(&self) -> impl Fn(f64) -> f64 + '_ {
+    // TODO: Compute the derivative directly
+    // type F2 = F<F<f64, f64>, f64>;
+    // |x: f64| (self.function)(F2::var(x)).deriv()
+
+    let fp = self.fp();
+    let h = 1e-9;
+    move |x: f64| (fp(x + h) - fp(x)) / h
+  }
+}
+
 const TESTS: [TestCase; 8] = [
   TestCase {
-    function: |x: f64| 0.72 * x - 1.0,
+    function: |x| 0.72 * x - 1.0,
     a: -5.0,
     b: 5.0,
     n: 4,
     low_precision: false,
   },
   TestCase {
-    function: |x: f64| 0.72 * x * x - 1.0,
+    function: |x| 0.72 * x * x - 1.0,
     a: -1.0,
     b: 5.0,
     n: 4,
     low_precision: false,
   },
   TestCase {
-    function: |x: f64| x.powi(3) - x + 0.5,
+    function: |x| x.powi(3) - x + 0.6,
     a: -2.0,
     b: 2.0,
     n: 4,
     low_precision: false,
   },
   TestCase {
-    function: |x: f64| x * x.exp() - 1.0,
+    function: |x| x * x.exp() - 1.0,
     a: 0.0,
     b: 2.0,
     n: 6,
     low_precision: false,
   },
   TestCase {
-    function: |x: f64| x - x.sin() - 1.2,
+    function: |x| x - x.sin() - 1.2,
     a: 0.0,
     b: TAU,
     n: 6,
     low_precision: false,
   },
   TestCase {
-    function: |x: f64| (x / 1e6) - (x / 1e6).sin() - 1.2,
+    function: |x| (x / 1e6) - (x / 1e6).sin() - 1.2,
     a: 0.0,
     b: 1e6 * TAU,
     n: 6,
     low_precision: false,
   },
   TestCase {
-    function: |x: f64| -x.powi(11) + 1e-10,
+    function: |x| -x.powi(11) + 1e-10,
     a: -1.0,
     b: 1.0,
     n: 12,
     low_precision: true,
   },
   TestCase {
-    function: |x: f64| (20.0 * x).sin() + 10.0 * x.tanh() + 1.0,
+    function: |x| (F1::cst(20.0) * x).sin() + 10.0 * x.tanh() + 1.0,
     a: -1.0,
     b: 1.0,
     n: 40,
@@ -75,7 +96,7 @@ const TESTS: [TestCase; 8] = [
 #[test]
 fn test_secant() {
   for case in TESTS {
-    let f = &case.function;
+    let f = &case.f();
 
     // A very crude initial guess
     let x = (case.a + case.b) / 2.0;
@@ -89,17 +110,14 @@ fn test_secant() {
 #[test]
 fn test_newton() {
   for case in TESTS {
-    let f = &case.function;
-
-    // TODO: Compute the derivative directly
-    let h = 1e-6;
-    let fp = &|x: f64| (f(x + h) - f(x)) / h;
+    let f = &case.f();
+    let fp = &case.fp();
 
     // A very crude initial guess
     let x = (case.a + case.b) / 2.0;
     let x = newtons_method(f, fp, x, f64::EPSILON);
 
-    let epsilon = if case.low_precision { 1e-10 } else { 1e-14 };
+    let epsilon = if case.low_precision { 1e-10 } else { 1e-15 };
     assert_abs_diff_eq!(f(x), 0.0, epsilon = epsilon);
   }
 }
@@ -107,18 +125,15 @@ fn test_newton() {
 #[test]
 fn test_laguerre() {
   for case in TESTS {
-    let f = &case.function;
-
-    // TODO: Compute the derivative directly
-    let h = 1e-6;
-    let fp = &|x: f64| (f(x + h) - f(x)) / h;
-    let fpp = &|x: f64| (fp(x + h) - fp(x)) / h;
+    let f = &case.f();
+    let fp = &case.fp();
+    let fpp = &case.fpp();
 
     // A very crude initial guess
     let x = (case.a + case.b) / 2.0;
     let x = laguerres_method(f, fp, fpp, 1.0, x, f64::EPSILON);
 
-    let epsilon = if case.low_precision { 1e-10 } else { 1e-13 };
+    let epsilon = if case.low_precision { 1e-10 } else { 1e-15 };
     assert_abs_diff_eq!(f(x), 0.0, epsilon = epsilon);
   }
 }
@@ -126,7 +141,7 @@ fn test_laguerre() {
 #[test]
 fn test_bisection() {
   for case in TESTS {
-    let f = &case.function;
+    let f = &case.f();
     let x = bisection(f, case.a, case.b, f64::EPSILON);
     assert_abs_diff_eq!(f(x), 0.0, epsilon = 1e-14);
   }
@@ -135,7 +150,7 @@ fn test_bisection() {
 #[test]
 fn test_itp() {
   for case in TESTS {
-    let f = &case.function;
+    let f = &case.f();
     let x = itp(f, case.a, case.b, f64::EPSILON);
     assert_abs_diff_eq!(f(x), 0.0, epsilon = 1e-14);
   }
@@ -144,7 +159,7 @@ fn test_itp() {
 #[test]
 fn test_cheb() {
   for case in TESTS {
-    let f = &case.function;
+    let f = &case.f();
 
     for n in case.n..case.n + 20 {
       let cheb = Cheb::new(f, case.a, case.b, n);
